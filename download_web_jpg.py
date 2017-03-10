9149617280a1062917e043d7c342da9d18c89ed2
JIAHAO LUO <2679229323@qq.com>
2017-03-10 21:48:47 +0000

    Update download_web_jpg.py

--- download_web_jpg.py
+++ download_web_jpg.py
@@ -4,7 +4,9 @@ Created on Mon Mar  6 20:38:57 2017
 
 @author: James
 """
-
+'''
+This is a help function for my crawler, which is to download a picture from its link and give it a name generated from random number from (1,100) 
+'''
 
 def download_web_jpg(url):
     import random
